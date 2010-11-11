1c8a464265dcaabe478f1a8f5059b00720ee1b4a
David Valdez <me@gnuget.org>
2010-11-11 04:23:20 -0600

    Adding a interface for the translatable fields
    Now the user can choice which custom fields will be keep sync betweens
    translations

--- Main.php
+++ Main.php
@@ -95,9 +95,9 @@ function get_info($post_id) {
 }
 
 //Keeping update the custom fields between translated posts
-add_action('mf_presave','mfplus_update_values',10,6);
+add_action('mf_presave','mfplus_update_values',10,7);
 
-function mfplus_update_values($field_meta_id,$name,$group_index,$field_index,$post_id,$value_field){
+function mfplus_update_values($field_meta_id,$name,$group_index,$field_index,$post_id,$value_field,$writepanel_id){
   global $wpdb;
 
   //looking for the translated version of the post
@@ -110,10 +110,117 @@ function mfplus_update_values($field_meta_id,$name,$group_index,$field_index,$po
     return true;
   }
 
+  /** getting the translatables fields  **/
+  if(!$translatables = get_option("mfplus_translatables")){
+    $translatables = array(); 
+  }
+
   //getting the meta_id  of the translated field
   foreach($ids as $value) {
-    $meta_id = $wpdb->get_var($wpdb->prepare("SELECT id FROM ".MF_TABLE_POST_META." WHERE post_id = {$value->element_id} AND field_name = '{$name}' AND group_count = {$group_index} AND field_count = {$field_index} AND order_id = {$group_index}"));
-    //updating the field value
-    update_post_meta($value->element_id,$name,$value_field); 
+
+    if(in_array($writepanel_id."_".$name,$translatables)){
+      //@todo use the GetMetaID for get the meta_id instead of this query 
+      $meta_id = $wpdb->get_var($wpdb->prepare("SELECT id FROM ".MF_TABLE_POST_META." WHERE post_id = {$value->element_id} AND field_name = '{$name}' AND group_count = {$group_index} AND field_count = {$field_index} AND order_id = {$group_index}"));
+      //updating the field value
+      update_post_meta($value->element_id,$name,$value_field); 
+    }
   }
+}
+
+/**
+ * Admin Menu
+ */
+add_action('admin_menu','mf_translatable_admin',20);
+
+
+function mfplus_translatable_fields_admin() {
+  global $wpdb;
+  
+
+  if(!$translatables = get_option("mfplus_translatables")) {
+    $translatables = array();
+  }
+  print "<h2>Custom Fields Translatables</h2>";
+  print "<div class='mf_plus'>";
+  print "<p>Select which Custom Fields will be keep sync between translated posts</p>";
+  print "</div>";
+
+  if($_GET['saved_translatable_fields'] == "true"): ?>
+      <div id="message" class="updated">
+        <strong>Translatable fields saved</strong>
+      </div>
+  <?php endif; 
+
+
+  //getting the write panels
+  $writepanels = RCCWP_CustomWritePanel::GetCustomWritePanels();
+  print "<form method=\"POST\">";
+  print "<div class='custom_fields'>";
+    foreach($writepanels as $writepanel) {
+      print "<h2>{$writepanel->name}</h2>";
+      //getting the custom fields of this write panel
+      $fields =  $wpdb->get_results(" SELECT  cf.id,cf.name,cf.description FROM  ".MF_TABLE_GROUP_FIELDS." as cf LEFT JOIN ".MF_TABLE_PANEL_GROUPS." as mg  ON (cf.group_id = mg.id) WHERE  mg.panel_id = {$writepanel->id}");
+      ?>
+      <table class="widefat" width="100%">
+        <thead>
+          <tr>
+            <th>Custom Field Name</th>
+            <th>Description</th>
+            <th>Keep sync</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($fields as $field):?>
+          <tr>
+            <td><?php print $field->name;?></td>
+            <td><?php print $field->description;?></td>
+            <td>
+              <?php 
+                $checked = "";
+                if(in_array($writepanel->id."_".$field->name,$translatables)){
+                  $checked = "checked=checked";
+                }
+              ?>
+              <input type="checkbox" name="translatable_field[<?php print $writepanel->id; ?>][<?php print $field->name;?>]" <?php print $checked;?>>
+            </td>
+          </tr>
+          <?php endforeach;?>
+        </tbody>
+      </table>
+      <?php
+    }
+
+  print "</div>";
+  print "<input type=\"submit\" name=\"save_submit\" id=\"save_sync\">";
+  print "</form>";
+}
+
+function mf_translatable_admin(){
+    add_submenu_page('MagicFieldsMenu','Translatable Fields','Translatable Fields',10,'translatablefields','mfplus_translatable_fields_admin');
+}
+
+
+/** Saving translatable fields **/
+add_action('init','save_translatable_fields');
+function save_translatable_fields(){
+  if(empty($_POST['translatable_field'])){
+    return false;
+  }
+
+  $translatables = array();
+  foreach($_POST['translatable_field'] as $writepanel_id => $fields) {
+    foreach($fields as $field => $value){
+      $translatables[] = $writepanel_id."_".$field;
+    }
+  }
+
+  update_option("mfplus_translatables",$translatables);
+
+  wp_safe_redirect(
+    add_query_arg(
+      'saved_translatable_fields',
+      'true',
+      wp_get_referer()
+    )
+  );
 }
