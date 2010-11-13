4b005a25f99717621f8ed09a556b8732d8392842
David Valdez <david@astrata.com.mx>
2010-11-12 19:44:38 -0600

    fix in the translatable posts

--- js/mfpluswpml_edit.js
+++ js/mfpluswpml_edit.js
@@ -1,3 +1,16 @@
+jQuery.expr[':'].regex = function(elem, index, match) {
+    var matchParams = match[3].split(','),
+        validLabels = /^(data|css):/,
+        attr = {
+            method: matchParams[0].match(validLabels) ? 
+                        matchParams[0].split(':')[0] : 'attr',
+            property: matchParams.shift().replace(validLabels,'')
+        },
+        regexFlags = 'ig',
+        regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
+    return regex.test(jQuery(elem)[attr.method](attr.property));
+}
+
 /**
  *  This could be more useful in a external file for 
  *  be used by anothers files  
@@ -23,14 +36,16 @@ jQuery.extend({
 jQuery(document).ready(function($) {
   //Getting the write panel id
   panel_id = parseInt($.getUrlVar('custom-write-panel-id'));
+  if(isNaN(panel_id)) {
+    panel_id =   $('input[name=rc-cwp-custom-write-panel-id]').val();
+  }
 
   //is a write panel post list?
   if(!isNaN(panel_id)) {
     //putting this on the translation links
-    $.each($('td.column-icl_translations').has('a[href*=post-new.php]'),function(index,item) {
-      anchor  = $(item).children();
-      href =  $(anchor).attr('href');
-      $(anchor).attr('href',href+'&custom-write-panel-id='+panel_id); 
+    $('a:regex(href, trid=[0-9]+)').each(function() {
+      var href = $(this).attr('href');
+      $(this).attr('href',href +='&custom-write-panel-id='+panel_id);
     });
   }
 });
