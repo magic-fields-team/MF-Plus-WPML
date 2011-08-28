f0140cb20e338093eb6a179dcc165bf44e32467c
David Valdez <me@gnuget.org>
2011-08-27 21:36:02 -0500

    improvement in a javascript file

--- js/mfpluswpml_edit.js
+++ js/mfpluswpml_edit.js
@@ -1,20 +1,4 @@
 /**
- * http://james.padolsey.com/javascript/regex-selector-for-jquery/
- */
-jQuery.expr[':'].regex = function(elem, index, match) {
-    var matchParams = match[3].split(','),
-        validLabels = /^(data|css):/,
-        attr = {
-            method: matchParams[0].match(validLabels) ? 
-                        matchParams[0].split(':')[0] : 'attr',
-            property: matchParams.shift().replace(validLabels,'')
-        },
-        regexFlags = 'ig',
-        regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
-    return regex.test(jQuery(elem)[attr.method](attr.property));
-}
-
-/**
  *  This could be more useful in a external file for 
  *  be used by anothers files  
  */
@@ -46,7 +30,7 @@ jQuery(document).ready(function($) {
   //is a write panel post list?
   if(!isNaN(panel_id)) {
     //putting this on the translation links
-    $('a:regex(href, trid=[0-9]+)').each(function() {
+    $('a[href*="trid="]').each(function() {    
       var href = $(this).attr('href');
       $(this).attr('href',href +='&custom-write-panel-id='+panel_id);
     });
